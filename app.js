de365f6f0dac037d323afd0b8f3afeda97efe3f8
Gopalakrishnan Ramasubbu <gopala.krishnan.ramasubbu@ericsson.com>
2025-12-04 13:12:28 +0100

    Test commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Sample application file
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+function calculateSum(a, b) {
+    return a + b;
+}
+
+module.exports = { greet, calculateSum };
